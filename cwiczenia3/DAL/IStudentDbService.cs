ca224a97d752bb0958dd1193f9ce0f1a0855cf35
Witu <s15035@pjwstk.edu.pl>
2021-01-28 16:33:43 +0100

    Zadanie 4

---
+++ cwiczenia3/DAL/IStudentDbService.cs
@@ -0,0 +1,18 @@
+﻿using cwiczenia3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cwiczenia3.DAL
+{
+    public interface IStudentDbService
+    {
+        public IEnumerable<Student> GetStudents();
+        public Student GetStudent(string index);
+        public int CreateStudent(Student newStudent);
+        public int DeleteStudent(string index);
+        public int UpdateStudent(Student reStudent);
+        public Enrollment Enrollments(string index);
+    }
+}
---
+++ cwiczenia3/DAL/SqlServerDbService.cs
@@ -0,0 +1,15 @@
+﻿using cwiczenia3.Models;
+using cwiczenia3.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace cwiczenia3.DAL
+{
+    public class SqlServerDbService
+    {
+    }
+}
